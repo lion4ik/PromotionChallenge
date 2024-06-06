799f940dfce3d22d8ed7a86190344b73d8ee6ced
lion4ik <mr.pushkarev1991@gmail.com>
2024-06-06 14:09:18 +0200

    coding challenge initial commit

--- app/src/main/java/com/alexey/cabifytestapp/ui/theme/Theme.kt
+++ app/src/main/java/com/alexey/cabifytestapp/ui/theme/Theme.kt
@@ -26,7 +26,7 @@ private val LightColorScheme = lightColorScheme(
 fun CabifyTestAppTheme(
         darkTheme: Boolean = isSystemInDarkTheme(),
         // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
+        dynamicColor: Boolean = false,
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
